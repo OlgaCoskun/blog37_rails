f56fcdc36426c1ddd02fdba2d012df12ebbaee3c
Olga Coskun <ovin999@gmail.com>
2017-10-10 19:11:16 +0300

    implement articles#edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,11 +14,26 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    if @article.valid?
+    if @article.save
       @article.save
       redirect_to @article
     else
       render action: 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      @article.save
+      redirect_to @article
+    else
+      render action: 'edit'
     end
   end
 
@@ -27,5 +42,6 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :text)
     end
+
 
 end
